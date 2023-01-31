76e5f64e6d038de7d59d9a0c6962f1c6d8e66fe4
Regielberson Alexandre Pereira Lirio <rlirio@supriservice.local>
2023-01-31 08:02:51 -0300

    melhorando a listagem

--- src/main/java/br/com/tarweb/model/Equipamento.java
+++ src/main/java/br/com/tarweb/model/Equipamento.java
@@ -8,15 +8,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Lob;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity(name="Equipamentos")
 public class Equipamento {
 	
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+
 	private String marca;
+
 	private String modelo;
+
 	private String serie;
 	
 	private String imagem;
